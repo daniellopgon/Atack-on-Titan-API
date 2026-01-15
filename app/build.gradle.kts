1c329ef2840cde6c125eaf61affda23fcd14e322
LGD <daniel.lopgon.4@educa.jcyl.es>
2026-01-15 18:53:38 +0100

    Commit(task/1/add-project-set-up):
    -Arreglado conflictos de versiones

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,11 +8,11 @@ plugins {
 
 android {
     namespace = "com.example.atackontitanapi"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.atackontitanapi"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
